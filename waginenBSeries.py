6fb93ce06c6b0c6b7785ec9ca49b950bc9ed669d
Andy Yu <ayu303@gatech.edu>
2026-07-21 19:41:51 -0400

    Convert the pitch-diameter ratio into a param

--- waginenBSeries.py
+++ waginenBSeries.py
@@ -35,12 +35,12 @@ class WaginenBSeries:
             #check array dimensions are correct, raise exception if not
             if len(self.torqueCorrectionCoefficients) != 13 or len(self.thrustCorrectionCoefficients[0]) != 6:
                 raise Exception("WaginenBSeriesThrust.csv improperly modified or corrupted! Try reinstalling or manually reentering coefficient values.")
-    def thrustCoefficient(self, d: float, Z: float, AEA0: float, p: float, J: float)->float:
+    def thrustCoefficient(self, pdRatio: float, Z: float, AEA0: float, J: float)->float:
         """
         Calculates the propeller thrust coefficient K_T of a propeller given basic propeller characteristics.
         These values are normalized for a Reynolds Number of 0.75. 
         Use thrustCoefficientCorrected to get the correct coefficient value for a full-scale reynolds number, or use the ITTC 1978 method.
-        d: propeller diameter. Diameter units must be the same as pitch units!
+        pdRatio: Pitch-diameter ratio.
         Z: number of propeller blades
         AEA0: expanded blade area ratio A_E/A_0
         p: propeller pitch. Pitch units must be the same as diameter units!
@@ -49,32 +49,30 @@ class WaginenBSeries:
         kT = 0
         for row in self.thrustRegressionCoefficients:
             a_i, b_i, c_i, d_i, e_i = row
-            kT += a_i * (J ** b_i) * ((p / d) ** c_i) * (AEA0 ** d_i) * (Z ** e_i)
+            kT += a_i * (J ** b_i) * ((pdRatio) ** c_i) * (AEA0 ** d_i) * (Z ** e_i)
         return kT
-    def torqueCoefficient(self, d: float, Z: int, AEA0: float, p: float, J: float)->float:
+    def torqueCoefficient(self, pdRatio: float, Z: int, AEA0: float, J: float)->float:
         """
         Calculates the propeller torque coefficient K_Q of a propeller given basic propeller characteristics.
         These values are normalized for a Reynolds Number of 0.75. 
         Use torqueCoefficientCorrected to get the correct coefficient value for a full-scale reynolds number, or use the ITTC 1978 method.
-        d: propeller diameter. Diameter units must be the same as pitch units!
+        pdRatio: Pitch-diameter ratio.
         Z: number of propeller blades
         p: propeller pitch. Pitch units must be the same as diameter units!
         AEA0: expanded blade area ratio A_E/A_0
-        PD: the pitch-diameter ration P/D
         J: the advance ratio J
         """
         kQ = 0
         for row in self.torqueRegressionCoefficients:
             a_i, b_i, c_i, d_i, e_i = row
-            kQ += a_i * (J ** b_i) * ((p / d) ** c_i) * (AEA0 ** d_i) * (Z ** e_i)
+            kQ += a_i * (J ** b_i) * ((pdRatio) ** c_i) * (AEA0 ** d_i) * (Z ** e_i)
         return kQ
-    def torqueCoefficientCorrected(self, d: float, Z: int, AEA0: float, p: float, J: float, reynolds: float)->float:
+    def torqueCoefficientCorrected(self, pdRatio: float, Z: int, AEA0: float, J: float, reynolds: float)->float:
         """
         Calculates the non-normalized propeller torque coefficient K_Q of a propeller given basic propeller characteristics and the reynolds number of the ship.
         Use torqueCoefficient to obtain normalized coefficient values to a reynolds number of 2e6.
-        d: propeller diameter. Diameter units must be the same as pitch units!
+        pdRatio: propeller diameter. Diameter units must be the same as pitch units!
         Z: number of propeller blades
-        p: propeller pitch. Pitch units must be the same as diameter units!
         AEA0: expanded blade area ratio A_E/A_0
         PD: the pitch-diameter ration P/D
         J: the advance ratio J
@@ -82,10 +80,10 @@ class WaginenBSeries:
         deltaKQ = 0
         for row in self.torqueCorrectionCoefficients:
             a_i, b_i, c_i, d_i, e_i, f_i = row
-            deltaKQ += a_i * (J ** b_i) * ((p/d) ** c_i) * (AEA0 ** d_i) * (Z ** e_i) * ((math.log10(reynolds) - 0.301) ** f_i)
-        kQ = self.torqueCoefficient(d, Z, AEA0, p, J) 
+            deltaKQ += a_i * (J ** b_i) * ((pdRatio) ** c_i) * (AEA0 ** d_i) * (Z ** e_i) * ((math.log10(reynolds) - 0.301) ** f_i)
+        kQ = self.torqueCoefficient(pdRatio, Z, AEA0, J) 
         return kQ + deltaKQ
-    def thrustCoefficientCorrected(self, d: float, Z: int, AEA0: float, p: float, J: float, reynolds: float)->float:
+    def thrustCoefficientCorrected(self, pdRatio: float, Z: int, AEA0: float, J: float, reynolds: float)->float:
         """
         Calculates the non-normalized propeller thrust coefficient K_T of a propeller given basic propeller characteristics and the reynolds number of the ship.
         Use torqueCoefficient to obtain normalized coefficient values to a reynolds number of 2e6.
@@ -99,22 +97,21 @@ class WaginenBSeries:
         deltaKT = 0
         for row in self.thrustCorrectionCoefficients:
             a_i, b_i, c_i, d_i, e_i, f_i = row
-            deltaKT += a_i * (J ** b_i) * ((p/d) ** c_i) * (AEA0 ** d_i) * (Z ** e_i) * ((math.log10(reynolds) - 0.301) ** f_i)
-        kT = self.torqueCoefficient(d, Z, AEA0, p, J) 
+            deltaKT += a_i * (J ** b_i) * ((pdRatio) ** c_i) * (AEA0 ** d_i) * (Z ** e_i) * ((math.log10(reynolds) - 0.301) ** f_i)
+        kT = self.torqueCoefficient(pdRatio, Z, AEA0, J) 
         return kT + deltaKT
 
 def main():
     """
     Test using ship example provided by Holtrop & Mennen 
     """
-    from shipSpeedCalc import pitch_calcs, j_calcs, reynolds_calcs, deltaCD_calcs, tc_calcs, chordLength_calcs
+    from shipSpeedCalc import j_calcs, reynolds_calcs, deltaCD_calcs, tc_calcs, chordLength_calcs
 
     bSeries = WaginenBSeries()
     #print(bSeries.thrustRegressionCoefficients)
     v = 12.8611 #velocity, 25 knots in m/s
     w = 0.2584 # wake fraction
     n = 1.6594 #propeller frequency (hertz)
-    p = pitch_calcs(v, w, n)
     diameter = 8
     print(p / diameter)
     propBlades = 4
@@ -133,11 +130,11 @@ def main():
     c_r = diameter * 0.520 * ae_ao  #chord length estimation at 0.7R, 0.520 is X2 for 4 blades
     reynolds = reynolds_calcs(c_r, v_r, KV)
     print(reynolds)
-    print("K_T: ", bSeries.thrustCoefficient(diameter, propBlades, ae_ao, p, j))
-    print("K_Q: ", bSeries.torqueCoefficient(diameter, propBlades, ae_ao, p, j))
+    print("K_T: ", bSeries.thrustCoefficient(pdRatio, propBlades, ae_ao, p, j))
+    print("K_Q: ", bSeries.torqueCoefficient(pdRatio, propBlades, ae_ao, p, j))
     print("Corrected thrust & torque coefficients")
-    print(bSeries.thrustCoefficientCorrected(diameter, propBlades, ae_ao, p, j, reynolds))
-    print(bSeries.torqueCoefficientCorrected(diameter, propBlades, ae_ao, p, j, reynolds))
+    print(bSeries.thrustCoefficientCorrected(pdRatio, propBlades, ae_ao, p, j, reynolds))
+    print(bSeries.torqueCoefficientCorrected(pdRatio, propBlades, ae_ao, p, j, reynolds))
 
     # Holtrop & Mennen correction:
     k_p = 0.00003
@@ -145,8 +142,8 @@ def main():
     deltaCD = deltaCD_calcs(tc, c_075, k_p)
     #k_t_ship = bSeries.thrustCoefficientCorrected(diameter, propBlades, ae_ao, p, 0, reynolds) + deltaCD * 0.3 * p * c_075 * propBlades / diameter ** 2
     #k_q_ship = bSeries.torqueCoefficientCorrected(diameter, propBlades, ae_ao, p, 0, reynolds) - deltaCD * 0.25 * c_075 * propBlades / diameter ** 2
-    k_t_ship = bSeries.thrustCoefficient(diameter, propBlades, ae_ao, p, j)
-    k_q_ship = bSeries.torqueCoefficient(diameter, propBlades, ae_ao, p, j)
+    k_t_ship = bSeries.thrustCoefficient(pdRatio, propBlades, ae_ao, p, j)
+    k_q_ship = bSeries.torqueCoefficient(pdRatio, propBlades, ae_ao, p, j)
     print("k_t_ship: ", k_t_ship)
     print("k_q_ship: ", k_q_ship)
 
